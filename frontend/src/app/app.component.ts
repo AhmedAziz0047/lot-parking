c5d42d162c7978805bfbb7b5a58c47b6a7098d41
AhmedAziz0047 <ahmedazizsouissi@gmail.com>
2023-07-09 11:30:12 +0100

    implementing user interface

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lot-parking';
+  vehicles=[
+          {
+              type:"car",
+              image:"../assets/car.jpg"
+          },{
+            type:"truck",
+            image:"../assets/truck.jpg"
+        },{
+          type:"bike",
+          image:"../assets/bike.jpg"
+      }
+        ]
 }
